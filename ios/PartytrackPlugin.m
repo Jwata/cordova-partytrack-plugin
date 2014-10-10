97341868bcc2538e19c4f18d78667b3cf524ee3b
Junji Watanabe <watanabe0621@gmail.com>
2014-10-10 14:39:56 +0900

    fixed

--- ios/PartytrackPlugin.m
+++ ios/PartytrackPlugin.m
@@ -7,7 +7,7 @@
 - (void)startWithAppID: (CDVInvokedUrlCommand*) command
 {
     CDVPluginResult* pluginResult = nil;
-    NSString* appId = [command.arguments objectAtIndex:0];
+    Int appId = [command.arguments objectAtIndex:0];
     NSString* appKey = [command.arguments objectAtIndex:1];
 
     [[Partytrack sharedInstance] startWithAppID:appId AndKey: appKey];
