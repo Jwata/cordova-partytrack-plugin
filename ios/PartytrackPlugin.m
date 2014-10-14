0a6e6cb469554f08ac90d344e73d7c020bfd39da
Junji Watanabe <watanabe0621@gmail.com>
2014-10-14 15:13:20 +0900

    Add debug message

--- ios/PartytrackPlugin.m
+++ ios/PartytrackPlugin.m
@@ -10,7 +10,12 @@
     int appId = [command.arguments objectAtIndex:0];
     NSString* appKey = [command.arguments objectAtIndex:1];
 
+    NSLog(@"appId %d", appId);
+    NSLog(@"appKey %@", appKey);
+
+    [[Partytrack sharedInstance] openDebugInfo];
     [[Partytrack sharedInstance] startWithAppID:appId AndKey: appKey];
+    [[Partytrack sharedInstance] openDebugInfo];
 
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
